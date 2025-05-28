1680584fef136ff38d74d4c768fbec350a2ed520
Pablo Guallichico <pablo.guallichico@outlook.com>
2025-05-27 21:48:18 -0500

    feat(api): add ProductDto, CreateProductDto, UpdateProductDto, and ProductDbContext with configurations

---
+++ api/src/Services/ProductService/ProductService.Infrastructure/Data/ProductDbContext.cs
@@ -0,0 +1,22 @@
+
+using Microsoft.EntityFrameworkCore;
+
+public class ProductDbContext : DbContext
+{
+    public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) { }
+    
+    public DbSet<Product> Products { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.Description).IsRequired().HasMaxLength(500);
+            entity.Property(e => e.Category).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.ImageUrl).HasMaxLength(255);
+            entity.Property(e => e.Price).HasColumnType("decimal(18,2)");
+        });
+    }
+}
