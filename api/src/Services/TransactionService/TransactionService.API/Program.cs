d2dac1ae06163246b935e62b51757249ba42287e
Pablo Guallichico <pablo.guallichico@outlook.com>
2025-05-28 13:31:15 -0500

    feat(api): update web host URL for transactions and products

--- api/src/Services/TransactionService/TransactionService.API/Program.cs
+++ api/src/Services/TransactionService/TransactionService.API/Program.cs
@@ -3,6 +3,7 @@ using FluentValidation.AspNetCore;
 using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://localhost:5185");
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
